a8794330df2c13cef82d314b6d37159374f5dace
Mathius Johnson <devachievem@gmail.com>
2020-09-20 22:07:04 -0700

    websocket implemented, db ready to respond to progress bar clicks

--- src/routes/progress_bar.js
+++ src/routes/progress_bar.js
@@ -1,9 +1,33 @@
 const router = require("express").Router();
 
-module.exports = (updatePoints) => {
+module.exports = (db) => {
   router.get("/points", (request, response) => {
-    db.query(`SELECT * FROM progress_bars`).then(({ rows: points }) => {
-      response.json();
+    db.query(`SELECT points FROM progress_bars`).then(({ rows: points }) => {
+      console.log("points: ", points);
+      response.json(points);
     });
   });
+
+  router.put("/points", (request, response) => {
+    if (process.env.TEST_ERROR) {
+      setTimeout(() => response.status(500).json({}), 1000);
+      return;
+    }
+
+    const { points } = request.body.points;
+    console.log("points: ", points);
+
+    db.query(
+      `
+      UPDATE progress_bars SET points = points + 1;
+      `
+    )
+      .then(() => {
+        setTimeout(() => {
+          response.status(204).json({});
+        }, 1000);
+      })
+      .catch((error) => console.log(error));
+  });
+  return router;
 };
