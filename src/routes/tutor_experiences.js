067305edab019b17d102afb628450b1a78b83948
Matthew Gibson <mattgibson6@gmail.com>
2020-09-27 14:42:16 -0600

    add queries for update and delete tutor sessions

--- src/routes/tutor_experiences.js
+++ src/routes/tutor_experiences.js
@@ -10,5 +10,37 @@ module.exports = (db) => {
     });
   });
 
+  router.put('/tutor_experiences/accept', (req, res) => {
+
+    const tutorExperienceID = req.body.tutorSessionID;
+    // console.log(tutorExperienceID);
+
+    db.query(`
+      UPDATE tutor_experiences
+      SET date_accepted = Now(),
+        status = 'in-progress'
+      WHERE id = $1
+      RETURNING *;
+    `, [tutorExperienceID])
+      .then(data => {
+        res.json(data);
+      })
+  });
+
+  router.put('/tutor_experiences/delete', (req, res) => {
+
+    const tutorExperienceID = req.body.tutorSessionID;
+    // console.log(tutorExperienceID);
+
+    db.query(`
+      DELETE FROM tutor_experiences
+      WHERE id = $1
+      RETURNING *;
+    `, [tutorExperienceID])
+      .then(data => {
+        res.json(data);
+      })
+  })
+
   return router;
 };
