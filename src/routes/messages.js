34e7ded1fd68471826c7f52389a0d87bbab1dfb2
Matthew Gibson <mattgibson6@gmail.com>
2020-10-01 18:31:43 -0600

    update messages table to include read entity

--- src/routes/messages.js
+++ src/routes/messages.js
@@ -1,12 +1,12 @@
 const router = require('express').Router();
 
 module.exports = db => {
-  // router.get('/messages', (req, res) => {
-  //   db.query(`SELECT * FROM messages;`)
-  //     .then((data) => {
-  //       res.json(data.rows)
-  //     });
-  // });
+  router.get('/messages', (req, res) => {
+    db.query(`SELECT * FROM messages;`)
+      .then((data) => {
+        res.json(data.rows)
+      });
+  });
 
   router.post('/messages/', (req, res) => {
 
