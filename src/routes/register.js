c780b5c20f5747b015c637e55effa9e97362949b
Angel <blacitea@gmail.com>
2020-10-28 11:52:19 -0700

    update query request to include last_name & first_name

--- src/routes/register.js
+++ src/routes/register.js
@@ -1,55 +1,62 @@
 const router = require('express').Router();
 
-module.exports = (db) => {
-  router.get('/register/avatars', (req, res) => {
-
-    db.query(`
+module.exports = db => {
+	router.get('/register/avatars', (req, res) => {
+		db.query(
+			`
       SELECT * FROM avatars;
-    `)
-      .then((data) => {
-        res.json(data.rows);
-      })
+    `
+		).then(data => {
+			res.json(data.rows);
+		});
+	});
 
-  });
-
-  router.get('/register/random_usernames', (req, res) => {
-
-    db.query(`
+	router.get('/register/random_usernames', (req, res) => {
+		db.query(
+			`
       SELECT * FROM random_usernames;
-    `)
-      .then((data) => {
-        res.json(data.rows);
-      })
+    `
+		).then(data => {
+			res.json(data.rows);
+		});
+	});
 
-  });
+	router.post('/register/new', (req, res) => {
+		const {
+			usernameInput,
+			firstnameInput,
+			lastnameInput,
+			emailInput,
+			passwordInput,
+			avatarSrc,
+		} = req.body;
 
-  router.post('/register/new', (req, res) => {
-
-    const { usernameInput, emailInput, passwordInput, avatarSrc } = req.body;
-
-    db.query(`
+		db.query(
+			`
       INSERT INTO users
-        (username, email, password, active)
+        (username, first_name, last_name, email, password, active)
       VALUES
-        ($1, $2, $3, true)
+        ($1, $2, $3, $4, $5, true)
       RETURNING id;
-    `, [usernameInput, emailInput, passwordInput])
-      .then(data => {
-        const user_id = data.rows[0].id;
-        res.json({ user_id })
-        db.query(`
+    `,
+			[usernameInput, firstnameInput, lastnameInput, emailInput, passwordInput]
+		).then(data => {
+			const user_id = data.rows[0].id;
+			res.json({ user_id });
+			db.query(
+				`
           INSERT INTO user_profiles
             (user_id, avatar, active)
           VALUES
             ($1, $2, true)
           RETURNING *;
-        `, [user_id, avatarSrc])
-          .then(data => {
-            console.log(data.rows);
-          })
-      })
+        `,
+				[user_id, avatarSrc]
+			).then(data => {
+				console.log(data.rows);
+			});
+		});
+	});
 
-  })
-
-  return router;
-}
+	return router;
+};
