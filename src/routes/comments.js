26a84a7bbc3c566c50531c635c62a5ddc216872a
Mathius Johnson <devachievem@gmail.com>
2021-02-01 22:03:13 -0800

    add console log to comments route

--- src/routes/comments.js
+++ src/routes/comments.js
@@ -70,8 +70,8 @@ module.exports = (db, updateComments) => {
   });
 
   router.delete("/comments", (req, res) => {
+    console.log("req.body", req.body);
     const query = JSON.parse(req.query.removeComment);
-    // console.log("req.body", req.body);
 
     const { post_id, commenter_id, id } = query;
     const params = [post_id, commenter_id];
