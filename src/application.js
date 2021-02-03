6233b18c768c3f1f0af122820a822a5039ab2f90
Mathius Johnson <devachievem@gmail.com>
2021-02-03 15:58:04 -0800

    add multiple origins to cors

--- src/application.js
+++ src/application.js
@@ -44,7 +44,21 @@ module.exports = function application(
   ENV,
   actions = { updateComments: () => {}, deleteComments: () => {} }
 ) {
-  app.use(cors());
+  var allowedOrigins = ['http://localhost:3002/', 'https://freeflow-network.netlify.app/']
+
+  app.use(cors({
+    origin: function(origin, callback){
+    // allow requests with no origin 
+      // (like mobile apps or curl requests)
+      if(!origin) return callback(null, true);
+        if(allowedOrigins.indexOf(origin) === -1){
+         var msg = 'The CORS policy for this site does not ' +
+        'allow access from the specified Origin.';
+        return callback(new Error(msg), false);
+        }
+      return callback(null, true);
+    }
+  }));
   app.use(helmet());
   app.use(bodyparser.json());
 
