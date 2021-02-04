1be24c1f5facfe3850ece07c710f183e909afe0d
Mathius Johnson <devachievem@gmail.com>
2021-02-04 08:55:21 -0800

    change posts route

--- src/routes/posts.js
+++ src/routes/posts.js
@@ -4,13 +4,11 @@ module.exports = (db) => {
   router.get("/posts", (req, res) => {
     db.query(
       `
-      SELECT posts.id as id, owner_id, text_body, users.first_name, users.last_name, time_posted, status_field, posts.active, users.active, db_skills.name as skill, db_skills.id as skill_id
+      SELECT posts.id as id, owner_id, text_body, users.first_name, users.last_name, time_posted, status_field, posts.active, users.active,
       FROM posts
       JOIN user_profiles ON user_profiles.id = owner_id
       JOIN users on users.id = owner_id
-      JOIN  posts_skills on posts_skills.post_id=posts.id JOIN db_skills on db_skills.id = posts_skills.db_skills_id
-      GROUP BY posts.id, user_profiles.id, users.id,db_skills.name,db_skills.id
-      ORDER BY time_posted DESC;
+      GROUP BY user_profiles.id, users.id ORDER BY time_posted DESC;
       `
     ).then((data) => {
       res.json(data.rows);
