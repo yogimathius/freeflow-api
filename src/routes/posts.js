123e2091b301d326ce4edc710df783a4e4429183
Mathius Johnson <devachievem@gmail.com>
2021-02-04 10:47:46 -0800

    change DASC to ADESC in posts route

--- src/routes/posts.js
+++ src/routes/posts.js
@@ -8,7 +8,7 @@ module.exports = (db) => {
       FROM posts
       JOIN user_profiles ON user_profiles.id = owner_id
       JOIN users on users.id = owner_id
-      GROUP BY posts.id, user_profiles.id, users.id ORDER BY time_posted;
+      GROUP BY posts.id, user_profiles.id, users.id ORDER BY time_posted DESC;
       `
     ).then((data) => {
       res.json(data.rows);
