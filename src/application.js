68a72349910eb31a5f91e3806459f537e1a74d04
Mathius Johnson <devachievem@gmail.com>
2021-02-04 09:33:30 -0800

    change cors setup to setHeader

--- src/application.js
+++ src/application.js
@@ -45,16 +45,24 @@ module.exports = function application(
   actions = { updateComments: () => {}, deleteComments: () => {} }
 ) {
   
-  var whitelist = ['https://freeflow-two-point-o.herokuapp.com/api/', 'http://example2.com']
-  var corsOptions = {
-    origin: function (origin, callback) {
-      if (whitelist.indexOf(origin) !== -1) {
-        callback(null, true)
-      } else {
-        callback(new Error('Not allowed by CORS'))
-      }
-    }
-  }
+  app.use(function (req, res, next) {
+
+    // Website you wish to allow to connect
+    res.setHeader('Access-Control-Allow-Origin', '*');
+ 
+    // Request methods you wish to allow
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+ 
+    // Request headers you wish to allow
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+ 
+    // Set to true if you need the website to include cookies in the requests sent
+    // to the API (e.g. in case you use sessions)
+    res.setHeader('Access-Control-Allow-Credentials', true);
+ 
+    // Pass to next layer of middleware
+    next();
+ });
      
   app.use(helmet());
   app.use(bodyparser.json());
@@ -74,7 +82,7 @@ module.exports = function application(
   app.use("/api", users(db));
   app.use("/api", register(db));
 
-  app.get("/", cors(corsOptions), (req, res) => {
+  app.get("/", (req, res) => {
     res.json({ message: "Welcome to freeflow application." });
   });
 
