3feec01ddddc4a5d75ba607b21d0860eaf83e2d0
Mathius <devachievem@gmail.com>
2021-06-27 21:59:56 -0700

    adjust messages route

--- src/routes/messages.js
+++ src/routes/messages.js
@@ -1,15 +1,29 @@
 const router = require('express').Router();
 
 module.exports = db => {
-  router.get('/messages', (req, res) => {
-    db.query(`SELECT * FROM messages;`)
+  router.get('/conversations/:id', (req, res) => {
+    const userId = req.params.id;
+
+    db.query(`
+      SELECT 
+        u1.id AS senderID,
+        concat(u1.first_name, ' ', u1.last_name) AS sender,
+        u2.id AS receiverID,
+        concat(u2.first_name, ' ', u2.last_name) AS receiver
+      FROM messages
+        JOIN users u1 ON u1.id = sender_id
+        JOIN users u2 ON u2.id = receiver_id
+      WHERE receiver_id = $1 OR sender_id = $1
+      GROUP BY u1.id, u2.id, sender, receiver;
+      `, [userId])
       .then(({rows: messages}) => {
         console.log('message gets hit: ', messages);
         res.json(
-          messages.reduce(
-            (previous, current) => ({ ...previous, [current.id]: current }),
-            {}
-          )
+          messages
+          // messages.reduce(
+          //   (previous, current) => ({ ...previous, [current.id]: current }),
+          //   {}
+          // )
         )
       });
   });
