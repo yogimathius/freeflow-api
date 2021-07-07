4e43e2ba84a5c2c7ba65314089677f46b3198937
Mathius <devachievem@gmail.com>
2021-07-06 20:17:31 -0700

    adjust messages route

--- src/routes/messages.js
+++ src/routes/messages.js
@@ -51,10 +51,6 @@ module.exports = db => {
   router.post('/messages/new', (req, res) => {
 
     const { receiverID, textInput, senderID } = req.body;
-    
-    console.log('receiver', receiverID);
-    console.log('sender', senderID);
-    console.log('textInput', textInput);
 
     const dateNow = new Date().toISOString();
     console.log(dateNow);
@@ -64,11 +60,21 @@ module.exports = db => {
         (sender_id, receiver_id, text_body, time_sent, active)
       VALUES
         ($1, $2, $3, $4, true)
-      RETURNING *;
+      SELECT 
+        messages.id AS id,
+        u1.id AS senderID,
+        concat(u1.first_name, ' ', u1.last_name) AS sender,
+        u2.id AS receiverID,
+        concat(u2.first_name, ' ', u2.last_name) AS receiver
+      FROM messages
+        JOIN users u1 ON u1.id = sender_id
+        JOIN users u2 ON u2.id = receiver_id
+      WHERE receiver_id = $2 AND sender_id = $1
+      GROUP BY u1.id, u2.id, sender, receiver;;
     `, [senderID, receiverID, textInput, dateNow])
       .then((data) => {
         console.log(data.rows);
-        res.json(data.rows[0]);
+        res.json(data.rows);
       });
 
 
