2b07817aa1b8a25ac2cd075d52cea06d8d71761f
Mathius <info@yogimathius.dev>
2023-03-09 22:33:53 -0800

    fix: add headers

--- src/application.js
+++ src/application.js
@@ -26,7 +26,13 @@ app.use(cors({
   origin: ['https://freeflow-network.netlify.app', 'http://localhost:3000'],
   credentials: true, 
 }));
-
+app.use(function (req, res, next) {
+  //Enabling CORS
+  res.header("Access-Control-Allow-Origin", ["https://freeflow-network.netlify.app"]);
+  res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, x-client-key, x-client-token, x-client-secret, Authorization");
+    next();
+});
 const cookieParser = require("cookie-parser");
 app.use(cookieParser());
 
